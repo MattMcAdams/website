cf72dd090ce4915114eb3219c0e3d230547636b6
Matthew McAdams <mattmcadams@outlook.com>
2020-03-29 11:18:52 -0500

    Add variable colors to git calendar

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,4 +1,4 @@
-<style>
+<style lang="scss">
 @media (orientation: landscape) {
   .sidebar-left {
     display: grid;
@@ -9,6 +9,38 @@
 .contrib-footer {
   margin: 0;
   padding: 0;
+}
+.js-calendar-graph {
+  width: 100%;
+  min-width: 700px;
+  float: right;
+}
+.graph-before-activity-overview {
+  overflow: scroll;
+  padding-top: 0;
+}
+.contrib-legend {
+  li[style*="#ebedf0"] { background-color: var(--color-gray-200) !important; }
+  li[style*="#c6e48b"] { background-color: var(--color-blue-200) !important; }
+  li[style*="#7bc96f"] { background-color: var(--color-blue-400) !important; }
+  li[style*="#239a3b"] { background-color: var(--color-blue-600) !important; }
+  li[style*="#196127"] { background-color: var(--color-blue-800) !important; }
+}
+.calendar-graph text.wday,
+.calendar-graph text.month {
+  fill: var(--color-gray-500);
+}
+.text-gray,
+.text-muted {
+  color: var(--color-gray-600);
+}
+.git-calendar .text-gray,
+.git-calendar .text-muted {
+  font-size: var(--fluid--1);
+}
+.contrib-number {
+  color: var(--color-gray-800) !important;
+  font-size: var(--fluid-2);
 }
 </style>
 
@@ -33,14 +65,14 @@
       <p>mattmcadams@outlook.com<br><a>Curriculum Vitae</a></p>
       </div>
     </aside>
-    <div>
+    <div style="min-width: 0;">
       <section>
+        <div class="git-calendar"></div>
+      </section>
+      <section style="margin-top: var(--space-fluid-half);">
         <div class="grid">
           <ProjectCard v-for="edge in $page.projects.edges" :key="edge.node.id" :project="edge.node"/>
         </div>
-      </section>
-      <section style="margin-top: var(--space-fluid-half);">
-        <div class="git-calendar"></div>
       </section>
     </div>
   </div>
@@ -76,7 +108,30 @@ export default {
   },
   mounted() {
     window.GitHubCalendar = require('github-calendar')
-    GitHubCalendar(".git-calendar", "mattmcadams", { responsive: true })
+    GitHubCalendar(".git-calendar", "mattmcadams", { responsive: true }).then(() => changeCalendarColors())
+
+    function changeCalendarColors() {
+      let days = document.getElementsByClassName("day")
+      console.log("days", days)
+
+      for (let i = 0; i < days.length; i++) {
+        console.log("day", days[i])
+        let day = days[i]
+        let count = day.getAttribute("data-count")
+
+        if (count == 0) {
+          day.setAttribute("fill", "var(--color-gray-200)")
+        } else if (count < 10) {
+          day.setAttribute("fill", "var(--color-blue-300)")
+        } else if (count < 20) {
+          day.setAttribute("fill", "var(--color-blue-400)")
+        } else if (count < 30) {
+          day.setAttribute("fill", "var(--color-blue-500)")
+        } else if (count > 30) {
+          day.setAttribute("fill", "var(--color-blue-600)")
+        }
+      }
+    }
   }
 }
 </script>
