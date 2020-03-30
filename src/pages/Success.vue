9a87d73e959fec208ae726af67f3026be0a2c0d9
Matthew McAdams <mattmcadams@outlook.com>
2020-03-30 00:12:56 -0500

    ad padding to success page

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -1,7 +1,9 @@
 <template>
 <layout>
-  <h1 style="text-align: center;">Thanks for your submission!</h1>
-  <p style="text-align: center;">I'll be in touch soon.</p>
-  <p style="text-align: center;"><a href="/" class="btn">Home</a></p>
+  <div class="section-pad">
+    <h1 style="text-align: center;">Thanks for your submission!</h1>
+    <p style="text-align: center;">I'll be in touch soon.</p>
+    <p style="text-align: center;"><a href="/" class="button">Home</a></p>
+  </div>
 </layout>
 </template>
