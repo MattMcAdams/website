4fafe41a3929689e10a307dfd872755aed9fa1f1
Matthew McAdams <mattmcadams@outlook.com>
2020-03-30 10:23:57 -0500

    Update toggle theme

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -2,23 +2,20 @@
 .toggle-theme {
   background-color: transparent;
   border: 0;
-  color: var(--body-color);
   cursor: pointer;
+  color: var(--color-gray-600) !important;
 
-  &:hover {
-    opacity: 0.8;
-  }
-
-  &:focus {
+  &:focus, &:hover {
     outline: none;
+    color: var(--color-gray-800) !important;
   }
 }
 </style>
 
 <template>
-  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme">
-      <svg v-if="darkTheme" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
-      <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme" style="font-size: inherit; padding: 0; padding-right: calc(var(--space) * 2)">
+    <span class="icon-sun" v-if="darkTheme"></span>
+    <span class="icon-moon" v-else></span>
   </button>
 </template>
 
@@ -31,9 +28,10 @@ export default {
   },
   methods: {
     toggleTheme() {
+      // Toggle the boolean
       this.darkTheme = !this.darkTheme
-
       // This is using a script that is added in index.html
+      // Uses inline if statement. If darkTheme return 'dark', else return 'light'
       window.__setPreferredTheme(
         this.darkTheme ? 'dark' : 'light'
       )
