0eef1861311f0c33c91e5dd1735a4e7f3e450bb9
Matthew McAdams <mattmcadams@outlook.com>
2020-04-24 09:47:12 -0500

    Create swatches component

---
+++ src/components/Swatches.vue
@@ -0,0 +1,30 @@
+<style>
+.colors {
+  display: flex;
+  margin-bottom: var(--space);
+}
+
+.color-swatch {
+  height: 50px;
+  width: 50px;
+  flex-grow: 1;
+}
+</style>
+
+<template>
+  <div class="colors mb-1">
+    <div class="color-swatch"
+      v-for="color in this.colors"
+      :key="color.id"
+      v-bind:style="{backgroundColor: color}">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    colors: Array
+  }
+}
+</script>
