a5f8dc8938616760fafa741a347f9765b44af9ac
Matthew McAdams <mattmcadams@outlook.com>
2020-04-24 09:45:42 -0500

    Move fonts to Gridsome head loader

--- src/main.js
+++ src/main.js
@@ -13,6 +13,12 @@ export default function (Vue, { router, head, isClient, appOptions }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
+  // Add google fonts
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Lato%7CRoboto+Slab&display=swap'
+  })
+
   // Add global state management for dark mode
   Vue.use(Vuex)
   appOptions.store = new Vuex.Store({
