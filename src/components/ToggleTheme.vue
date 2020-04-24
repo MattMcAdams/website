ffe4cdeefe00181066b59fed144d44df1dc108b2
Matthew McAdams <mattmcadams@outlook.com>
2020-04-24 14:36:11 -0500

    Clean up ToggleTheme
    Remove aggressive styles and console logs

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -3,17 +3,14 @@
   background-color: transparent;
   border: 0;
   cursor: pointer;
-  color: var(--color-gray-600) !important;
-
   &:focus, &:hover {
     outline: none;
-    color: var(--color-gray-800) !important;
   }
 }
 </style>
 
 <template>
-  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme" style="font-size: inherit; padding: 0; padding-right: calc(var(--space) * 2)">
+  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme" style="font-size: inherit; padding: 0;">
     <span class="icon-sun" v-if="this.$store.state.darkTheme"></span>
     <span class="icon-moon" v-else></span>
   </button>
@@ -25,7 +22,6 @@ export default {
     toggleTheme() {
       // Toggle the boolean
       this.$store.commit('toggleTheme')
-      console.log("Dark Theme", this.$store.state.darkTheme)
       // This is using a script that is added in index.html
       // Uses inline if statement. If darkTheme return 'dark', else return 'light'
       window.__setPreferredTheme(
@@ -35,7 +31,6 @@ export default {
   },
   mounted() {
     if (window.__theme == 'dark') this.$store.commit('setDarkTheme', true)
-    console.log("Dark Theme", this.$store.state.darkTheme)
   }
 }
 </script>
