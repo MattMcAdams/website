02b6219ed5ee93970a2b76ef315ec8bab44c7044
Matthew McAdams <mattmcadams@outlook.com>
2020-04-24 09:45:15 -0500

    Add Vuex store for dark mode

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -14,31 +14,28 @@
 
 <template>
   <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme" style="font-size: inherit; padding: 0; padding-right: calc(var(--space) * 2)">
-    <span class="icon-sun" v-if="darkTheme"></span>
+    <span class="icon-sun" v-if="this.$store.state.darkTheme"></span>
     <span class="icon-moon" v-else></span>
   </button>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      darkTheme: false
-    }
-  },
   methods: {
     toggleTheme() {
       // Toggle the boolean
-      this.darkTheme = !this.darkTheme
+      this.$store.commit('toggleTheme')
+      console.log("Dark Theme", this.$store.state.darkTheme)
       // This is using a script that is added in index.html
       // Uses inline if statement. If darkTheme return 'dark', else return 'light'
       window.__setPreferredTheme(
-        this.darkTheme ? 'dark' : 'light'
+        this.$store.state.darkTheme ? 'dark' : 'light'
       )
     }
   },
   mounted() {
-    if (window.__theme == 'dark') this.darkTheme = true
+    if (window.__theme == 'dark') this.$store.commit('setDarkTheme', true)
+    console.log("Dark Theme", this.$store.state.darkTheme)
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,30 @@
 import '~/assets/style/_index.scss'
 import '~/assets/icomoon/style.css'
 
+import Vuex from 'vuex'
+
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { router, head, isClient, appOptions }) {
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  // Add global state management for dark mode
+  Vue.use(Vuex)
+  appOptions.store = new Vuex.Store({
+    state: {
+      darkTheme: false
+    },
+    mutations: {
+      toggleTheme (state) {
+        state.darkTheme = !state.darkTheme
+      },
+      setDarkTheme (state, bool) {
+        state.darkTheme = bool
+      }
+    }
+  })
 }
