476577945c7b8abac9bacf8270817ec5ce5434cb
Matthew McAdams <mattmcadams@outlook.com>
2020-04-26 00:02:31 -0500

    Add punctuation

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -6,7 +6,7 @@
     </div>
     <div style="align-self: center;">
       <h1>Thanks!</h1>
-      <p>Your message has been successfully sent. Thank you for your interest in my work and for reaching out to me</p>
+      <p>Your message has been successfully sent. Thank you for your interest in my work and for reaching out to me.</p>
       <p>I’ll be in touch soon!</p>
       <p><a href="/" class="button">Return Home</a></p>
     </div>
