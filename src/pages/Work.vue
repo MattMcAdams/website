f42cb31db551c73da818eef1a66c8e31f0447df7
Matthew McAdams <mattmcadams@outlook.com>
2020-08-27 15:59:56 -0500

    Update Work.vue
    This will now include a few featured github repos and headers for each section

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -18,9 +18,50 @@
   </template>
 
   <section>
+    <h2 class="mt-0 txt-h3">Open Source</h2>
     <GitCalendar/>
+    <div class="grid">
+      <GitCard
+        :title="'mattmcadams.com'"
+        :path="'https://github.com/MattMcAdams/website'"
+        :description="'My personal website, blog, and portfolio'"
+        :languages="[
+          {'name':'HTML', 'percent':'34%', 'width':'100%'},
+          {'name':'Vue', 'percent':'17%', 'width':'66%'},
+          {'name':'CSS', 'percent':'49%', 'width':'49%'}
+        ]"
+      />
+      <GitCard
+        :title="'UABIT Linear'"
+        :path="'https://github.com/UAB-IT/linear'"
+        :description="'JavaScript and CSS from the uab.edu/it sites'"
+        :languages="[
+          {'name':'JavaScript', 'percent':'11%', 'width':'100%'},
+          {'name':'CSS', 'percent':'89%', 'width':'89%'}
+        ]"
+      />
+      <!-- <GitCard
+        :title="'UABIT Projects'"
+        :path="'https://github.com/UAB-IT/Projects'"
+        :description="'Miscellaneous code mini projects'"
+        :languages="[
+          {'name':'JavaScript', 'percent':'28%', 'width':'100%'},
+          {'name':'CSS', 'percent':'16%', 'width':'72%'},
+          {'name':'HTML', 'percent':'56%', 'width':'56%'}
+        ]"
+      />
+      <GitCard
+        :title="'Stratus SCSS'"
+        :path="'https://github.com/MattMcAdams/stratus'"
+        :description="'Sass toolset for typography, color, and more'"
+        :languages="[
+          {'name':'CSS', 'percent':'100%', 'width':'100%'}
+        ]"
+      /> -->
+    </div>
   </section>
   <section class="mt-1-fluid">
+    <h2 class="txt-h3">Art &amp; Design</h2><br>
     <div class="grid">
       <ImageCard v-for="edge in $page.projects.edges"
       :key="edge.node.id"
@@ -54,6 +95,8 @@ query {
 </page-query>
 
 <script>
+
+import GitCard from '~/components/GitCard.vue'
 import ImageCard from '~/components/ImageCard.vue'
 import GitCalendar from '~/components/GitCalendar.vue'
 
@@ -63,7 +106,8 @@ export default {
   },
   components: {
     ImageCard,
-    GitCalendar
+    GitCalendar,
+    GitCard
   }
 }
 </script>
